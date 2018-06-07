77a52927d323362cd49e2ca93d947ceb1f3d73f1
Oscar Spencer <oscar@tidelift.com>
2018-06-07 02:29:08 -0400

    Make page layout better on larger screens

--- themes/grain/source/js/main.js
+++ themes/grain/source/js/main.js
@@ -1,5 +1,5 @@
 const sidebarToggle = document.getElementById('sidebar-toggle');
-const sidebar = document.getElementById('sidebar');
+const sidebar = document.getElementById('sidebar-mobile');
 const content = document.getElementById('page-content-wrapper');
 sidebarToggle.addEventListener('click', () => {
   if (sidebar.classList.contains('sidebar-toggled') || content.classList.contains('sidebar-toggled')) {
