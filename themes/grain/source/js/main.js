30fc752c13995208da497bd804555c2646281f32
Oscar Spencer <oscar@tidelift.com>
2020-05-03 22:50:38 -0400

    Show next/previous page, table of contents

--- themes/grain/source/js/main.js
+++ themes/grain/source/js/main.js
@@ -1,6 +1,6 @@
 const sidebarToggle = document.getElementById('sidebar-toggle');
 const sidebar = document.getElementById('sidebar-mobile');
-const content = document.getElementById('page-content-wrapper');
+const content = document.getElementById('all-content-wrapper');
 if (sidebarToggle) sidebarToggle.addEventListener('click', () => {
   if (sidebar.classList.contains('sidebar-toggled') || content.classList.contains('sidebar-toggled')) {
     sidebar.classList.remove('sidebar-toggled');
@@ -10,3 +10,5 @@ if (sidebarToggle) sidebarToggle.addEventListener('click', () => {
     content.classList.add('sidebar-toggled');
   }
 });
+
+new ActiveMenuLink.default('.sidebar.toc', { activeClass: 'current', headerHeight: 0 });
