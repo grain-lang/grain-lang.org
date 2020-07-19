ac25e1b81bcec01e2e5c752a512ea4f875850b1b
Oscar Spencer <oscar@tidelift.com>
2020-07-19 00:16:28 -0400

    Initial blog

--- themes/grain/scripts/helpers.js
+++ themes/grain/scripts/helpers.js
@@ -1,4 +1,5 @@
 const _ = require('lodash')
+const authors = require('./authors')
 
 hexo.extend.helper.register('next', function (site, page) {
   let all = _.flatten(_.values(hexo.config.docs_groups))
@@ -12,4 +13,8 @@ hexo.extend.helper.register('prev', function (site, page) {
   let path = page.path.replace(/\.html$/, '')
   let prev = all[_.findIndex(all, p => p === path) - 1]
   return _.find(site.pages.data, p => p.path === prev + '.html')
+});
+
+hexo.extend.helper.register('getAuthor', function (name) {
+  return authors[name]
 });
