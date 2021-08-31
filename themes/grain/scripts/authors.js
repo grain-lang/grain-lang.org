98a67f6ad4cb63b66e7677e1bd560948e9a370a3
Marcus Roberts <m.roberts@nittygritty.net>
2021-08-31 18:48:06 +0100

    Added author profile and headshot

--- themes/grain/scripts/authors.js
+++ themes/grain/scripts/authors.js
@@ -8,9 +8,16 @@ module.exports = {
   },
   'Blaine Bublitz': {
     name: 'Blaine Bublitz',
-    title: 'Grain core team & tooling enthuiast',
+    title: 'Grain core team & tooling enthusiast',
     headshot: '/blog/photos/headshot/blaine-headshot-square.jpg',
     twitter: 'blainebublitz',
     github: 'phated',
+  },
+  'Marcus Roberts': {
+    name: 'Marcus Roberts',
+    title: 'Grain core team, loves to make the developer experience better',
+    headshot: '/blog/photos/headshot/marcus-headshot-square.jpg',
+    twitter: 'marcusr',
+    github: 'marcusroberts',
   }
 }
