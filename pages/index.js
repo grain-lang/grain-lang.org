d5c27048444d1041bf0e7c79f31d5a747c352b2d
Oscar Spencer <oscar@grain-lang.org>
2023-03-29 19:04:57 +0300

    bootstrap tailwind

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function HomePage() {
-  return <div>Welcome to the Grain website, v2!</div>;
+  return (
+    <div className="text-3xl font-bold underline">
+      Welcome to the Grain website, v2!
+    </div>
+  );
 }
 
 export default HomePage;
