1a1389ed8c62d0d256904ef66e30eb2f84640666
Oscar Spencer <oscar@grain-lang.org>
2023-03-31 19:59:06 +0300

    nav

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,73 @@
+import localFont from "next/font/local";
 import "../styles/global.css";
 
+import { ThemeContext } from "../hooks/useTheme";
+import { useEffect, useState, useCallback } from "react";
+
+const soehne = localFont({
+  src: [
+    {
+      path: "../public/fonts/soehne-leicht.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/soehne-buch.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/soehne-kraftig.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-soehne",
+});
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState("system");
+  const [darkMode, setDarkMode] = useState();
+
+  const wrappedSetTheme = useCallback(
+    (theme) => {
+      if (["light", "dark"].includes(theme)) {
+        localStorage.theme = theme;
+      } else {
+        localStorage.removeItem("theme");
+      }
+      setTheme(theme);
+    },
+    [setTheme]
+  );
+
+  useEffect(() => {
+    if (localStorage.theme === "dark") {
+      setTheme("dark");
+    } else if (localStorage.theme === "light") {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    setDarkMode(
+      theme === "dark" ||
+        (theme === "system" &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+    );
+  }, [theme, setDarkMode]);
+
+  return (
+    <ThemeContext.Provider value={[theme, wrappedSetTheme]}>
+      <main
+        className={`${
+          soehne.variable
+        } font-sans antialiased tracking-wide bg-gray-10 ${
+          darkMode ? "dark bg-purple-90" : ""
+        }`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </ThemeContext.Provider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Nav from "../components/nav";
+
 function HomePage() {
   return (
-    <div className="text-3xl font-bold underline">
-      Welcome to the Grain website, v2!
+    <div>
+      <Nav />
     </div>
   );
 }
