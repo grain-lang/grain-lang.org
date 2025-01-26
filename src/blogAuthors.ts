3e1d5d3a576325b4803c691adc2e67c25738295c
Alex Snezhko <alexsnezhko89@gmail.com>
2025-01-26 15:07:27 -0800

    Social media links, fix rss link

--- src/blogAuthors.ts
+++ src/blogAuthors.ts
@@ -6,9 +6,16 @@ import philipHeadshot from "./images/headshots/philip-headshot-square.jpg";
 import blaineHeadshot from "./images/headshots/blaine-headshot-square.jpg";
 import marcusHeadshot from "./images/headshots/marcus-headshot-square.jpg";
 
-export const authorInfo: Record<BlogAuthor, { xLink: string; github: string; headshot: ImageMetadata }> = {
-  "Oscar Spencer": { xLink: "oscar_spen", github: "ospencer", headshot: oscarHeadshot },
-  "Philip Blair": { xLink: "Philip_E_Blair", github: "peblair", headshot: philipHeadshot },
-  "Blaine Bublitz": { xLink: "BlaineBublitz", github: "phated", headshot: blaineHeadshot },
-  "Marcus Roberts": { xLink: "marcusr", github: "marcusroberts", headshot: marcusHeadshot },
+export interface AuthorInfo {
+  mastodonLink?: string;
+  blueskyProfile?: string;
+  githubProfile: string;
+  headshot: ImageMetadata;
+}
+
+export const authorInfoByName: Record<BlogAuthor, AuthorInfo> = {
+  "Oscar Spencer": { blueskyProfile: "ospencer.dev", githubProfile: "ospencer", headshot: oscarHeadshot },
+  "Philip Blair": { githubProfile: "peblair", headshot: philipHeadshot },
+  "Blaine Bublitz": { mastodonLink: "https://fosstodon.org/@phated", githubProfile: "phated", headshot: blaineHeadshot },
+  "Marcus Roberts": { githubProfile: "marcusroberts", headshot: marcusHeadshot },
 };
